bbdd5ce5e1aa8682c45a01668e2452f287423340
dindin-glitch <odinbeuchat.ob@gmail.com>
2022-01-03 23:38:30 +0100

    ajout de fonction de recherche de cookie

--- main.js
+++ main.js
@@ -22,3 +22,19 @@ function selectOption (myself) {
         document.getElementsByClassName('checkout-prompt').item(0).style.display = "none"
     }
 }
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
